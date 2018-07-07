87b0ebb8ab3e0e36dec80b7920ac3f9914fe91f9
Shane Cunningham <ShaneCunninnghamlive.ie>
2018-07-07 19:37:58 +0100

    updated js and css

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -7,8 +7,8 @@
 <title>{{$title}}</title>
 
 <!-- Bootstrap core CSS -->
-<link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">
+{{--<link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">--}}
 
 <!-- Custom styles for this template -->
 <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
-<link href="{{asset('css/app.css')}}" rel="stylesheet">
+<link href="{{asset('/css/app.css')}}" rel="stylesheet">
